7186d809e842e0a12990bffb3f4e6a1490bb0419
heniarmoniyem <henokmesfin2121@gmail.com>
2021-11-30 18:57:33 +0000

    pint added

---
+++ pint.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+/**
+ * pint - prints value at top of the stack
+ * @stack: doubly linked list
+ * @ln: line number
+ */
+void pint(stack_t **stack, unsigned int ln)
+{
+  if (!stack || !(*stack))
+    {
+      fprintf(stderr, "L%d: can't pint, stack empty\n", ln);
+      exit(EXIT_FAILURE);
+    }
+  printf("%d\n", (*stack)->n);
+}
