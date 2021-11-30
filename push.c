8272722c5fd38c8993cf802e610ac3f30e2855fd
heniarmoniyem <henokmesfin2121@gmail.com>
2021-11-30 18:16:43 +0000

    added queue functionality

---
+++ push.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+
+/**
+ * push - push element into the stack
+ * @stack: doubly linked list to store data
+ * @ln: line number
+ */
+void push(stack_t **stack, unsigned int ln)
+{
+  char *n = global.argument;
+
+  if ((is_digit(n)) == 0)
+    {
+      fprintf(stderr, "L%d: usage: push integer\n", ln);
+      exit(EXIT_FAILURE);
+    }
+
+  if (global.data_struct == 1)
+    {
+      if (!add_node(stack, atoi(global.argument)))
+	{
+	  fprintf(stderr, "Error: malloc failed\n");
+	  exit(EXIT_FAILURE);
+	}
+    }
+  else
+    {
+      if (!add_node_queue(stack, atoi(global.argument)))
+	{
+	  fprintf(stderr, "Error: malloc failed\n");
+	  exit(EXIT_FAILURE);
+	}
+    }
+}
