8fa36ad3883803f2ddfbafe82911b770c2ee6e9a
avocadoclouds <rehana447@gmail.com>
2021-12-01 10:26:40 -0800

    update

--- monty.h
+++ monty.h
@@ -64,4 +64,9 @@ extern globals global_vars;
  * 2 - more than one
  */
 
+void pop(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+
 #endif
