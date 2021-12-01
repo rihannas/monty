14e21f8a796435a7bc79e588834d270f213863f8
avocadoclouds <rehana447@gmail.com>
2021-12-01 05:35:53 -0800

    a function that does nothing

---
+++ nop.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * nop - does literally nothing
+ * @stack: double pointer to the head of the stack
+ * @line_number: the number of the line in file
+ *
+ * Return: void
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+}
