5390b9d5838e14c9a91b9777700f8b5c5cd767a8
avocadoclouds <rehana447@gmail.com>
2021-12-01 05:29:52 -0800

    swap function

---
+++ swap.c
@@ -0,0 +1,35 @@
+#include "monty.h"
+
+/**
+ * swap - swaps 2 elements in a stack
+ * @stack: souble pointer to the head of the stack
+ * @line_number: the number of the line in file
+ *
+ * Return: void
+ */
+
+void swap(stack_t **stack, unsigned int line_number)
+{
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+	{
+		printf("L%u: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE)
+	}
+
+	stack_t *tmp;
+
+	tmp = (*stack)->next;
+	(*stack)->prev = tmp;
+	(*stack)->next = tmp;
+
+	tmp->prev = NULL;
+
+	if (tmp->next != NULL)
+	{
+		tmp->next->prev = *stack;
+	}
+
+	tmp->next = *stack;
+	*stack = tmp;
+
+}
