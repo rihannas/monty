0f64677c65e8f34e68a1fd5d69be324ecfe449a6
heniarmoniyem <henokmesfin2121@gmail.com>
2021-12-02 16:58:25 +0000

    Pall Updated

--- pall.c
+++ pall.c
@@ -9,13 +9,13 @@
  */
 void _pall(stack_t **stack, unsigned int line_number)
 {
-	stack_t *current;
+  stack_t *current;
 
-	(void) line_number;
-	current = *stack;
-	while (current != NULL)
-	{
-		printf("%d\n", current->n);
-		current = current->next;
-	}
+  (void) line_number;
+  current = *stack;
+  while (current != NULL)
+    {
+      printf("%d\n", current->n);
+      current = current->next;
+    }
 }
