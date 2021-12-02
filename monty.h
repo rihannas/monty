f4d6e5d069ed92b991abc123545b036868351fe0
heniarmoniyem <henokmesfin2121@gmail.com>
2021-12-02 16:50:32 +0000

    New month Header

--- monty.h
+++ monty.h
@@ -17,9 +17,9 @@
  */
 typedef struct stack_s
 {
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
+  int n;
+  struct stack_s *prev;
+  struct stack_s *next;
 } stack_t;
 
 /**
@@ -32,8 +32,8 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
+  char *opcode;
+  void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 extern char stack_queue;
