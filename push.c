fdf91cfac315da0dcef9ad00de841ea317bc474c
avocadoclouds <rehana447@gmail.com>
2021-12-02 06:28:26 -0800

    final implementation

--- push.c
+++ push.c
@@ -1,34 +1,37 @@
 #include "monty.h"
 
 /**
- * push - push element into the stack
- * @stack: doubly linked list to store data
- * @ln: line number
+ * push - pushes new node to the end of the stack
+ * @stack: double pointer to the head of the stack
+ * @line_number: the number of a line of the file
+ *
+ * Return: void
  */
-void push(stack_t **stack, unsigned int ln)
+void push(stack_t **stack, unsigned int line_number)
 {
-  char *n = global.argument;
+	stack_t *node;
+	char *arg;
 
-  if ((is_digit(n)) == 0)
-    {
-      fprintf(stderr, "L%d: usage: push integer\n", ln);
-      exit(EXIT_FAILURE);
-    }
+	arg = strtok(NULL, "\n \r\t");
+	if (arg == NULL)
+	{
+		fprintf(stderr,"L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-  if (global.data_struct == 1)
-    {
-      if (!add_node(stack, atoi(global.argument)))
+	node = malloc(sizeof(stack_t));
+	if (node == NULL)
 	{
-	  fprintf(stderr, "Error: malloc failed\n");
-	  exit(EXIT_FAILURE);
+		printf("Error: malloc failed\n");
+		exit(EXIT_FAILURE);
 	}
-    }
-  else
-    {
-      if (!add_node_queue(stack, atoi(global.argument)))
-	{
-	  fprintf(stderr, "Error: malloc failed\n");
-	  exit(EXIT_FAILURE);
-	}
-    }
+
+	node->n = atoi(arg);
+	node->prev = NULL;
+	node->next = *stack;
+
+	if (*stack != NULL)
+		(*stack)->prev = node;
+
+	*stack = node;
 }
