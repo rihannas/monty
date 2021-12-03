c0baa584755059e776ab999a8a5d7fe349ad52c6
avocadoclouds <rehana447@gmail.com>
2021-12-02 18:32:35 -0800

    check

--- monty.h
+++ monty.h
@@ -7,6 +7,7 @@
 #include <ctype.h>
 #include <string.h>
 
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -36,6 +37,17 @@ typedef struct instruction_s
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+extern stack_t **global_head;                                                                                      
+                                                                                                                   
+void global_free(void);                                                                                            
+                                                                                                                   
+void read_file(char *file, stack_t **stack);                                                                       
+void parse_command(stack_t **stack, char *op, unsigned int line_num);                                              
+
+
+
+
 
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
